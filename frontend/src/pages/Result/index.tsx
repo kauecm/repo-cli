6310497b42cadefb38c4f4df7e11f8f0300ddd41
Kaue Costa <kauecmoraes@hotmail.com>
2022-01-27 15:59:08 -0300

    correções modules, ajustes em tabelas e icons

--- frontend/src/pages/Result/index.tsx
+++ frontend/src/pages/Result/index.tsx
@@ -1,29 +1,34 @@
 import Footerbar from "components/Footerbar";
 import Navbar from "components/NavBar";
+import { Link } from "react-router-dom";
 import './styles.css';
+import {ReactComponent as DownloadIcon} from "img/download-icon.svg";
 
-function Result(){
-    return(
-    <>
-    <Navbar/>
-    <div className="container">
-        <div>
-            <h1>Resultado:</h1>
-        </div>
-        <hr/>
-    
-        <div className="repo-result-clientes">
-        <p>Nome cliente:</p><p> NOME DO CLIENTE AQUI</p>
-        </div>
-        <div className="repo-result-clientes">
-        <p>Descrição:</p><p>DESCRIÇÃO DO CLIENTE</p>
-        </div>
-        <div className="repo-result-clientes">
-        <p>Tipo Cliente:</p><p>Tipo do cliente</p>
-        </div>
-        <hr/>
-        <div className="table-responsive table-pagination">
-                    <table className="table table-sm ">
+function Result() {
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <div className="button-voltar">
+                    <h1>Resultado:</h1>
+                    <Link to="/clientes">
+                    <button type="submit" className="btn btn-primary repo-btn">Voltar</button>
+                    </Link>
+                </div>
+                <hr />
+
+                <div className="repo-result-clientes">
+                    <p>Nome cliente:</p><p> NOME DO CLIENTE AQUI</p>
+                </div>
+                <div className="repo-result-clientes">
+                    <p>Descrição:</p><p>DESCRIÇÃO DO CLIENTE</p>
+                </div>
+                <div className="repo-result-clientes">
+                    <p>Tipo Cliente:</p><p>Tipo do cliente</p>
+                </div>
+                <hr />
+                <div className="table-responsive table-pagination">
+                    <table className="table table-sm table-style">
                         <thead>
                             <tr>
                                 <th>Id</th>
@@ -40,90 +45,87 @@ function Result(){
                                 <td>MF4 - Movimentação atipica </td>
                                 <td>Postgresql</td>
                                 <td>Regra</td>
-                                <td>www.linkParaDownloadDaRegra.com.br</td>
+                                <td><DownloadIcon/></td>
                             </tr>
                         </tbody>
                         <tbody >
                             <tr>
                                 <td>1</td>
-                                <td>MF6 - Movimentação atipica com contraparte</td>
+                                <td>MF4 - Movimentação atipica </td>
                                 <td>Postgresql</td>
                                 <td>Regra</td>
-                                <td>www.linkParaDownloadDaRegra.com.br</td>
+                                <td><DownloadIcon/></td>
                             </tr>
                         </tbody>
                         <tbody >
                             <tr>
                                 <td>1</td>
-                                <td>MF6 - Movimentação atipica com contraparte</td>
+                                <td>MF4 - Movimentação atipica </td>
                                 <td>Postgresql</td>
                                 <td>Regra</td>
-                                <td>www.linkParaDownloadDaRegra.com.br</td>
+                                <td><DownloadIcon/></td>
                             </tr>
                         </tbody>
                         <tbody >
                             <tr>
                                 <td>1</td>
-                                <td>MF6 - Movimentação atipica com contraparte</td>
+                                <td>MF4 - Movimentação atipica </td>
                                 <td>Postgresql</td>
                                 <td>Regra</td>
-                                <td>www.linkParaDownloadDaRegra.com.br</td>
+                                <td><DownloadIcon/></td>
                             </tr>
                         </tbody>
                         <tbody >
                             <tr>
                                 <td>1</td>
-                                <td>MF6 - Movimentação atipica com contraparte</td>
+                                <td>MF4 - Movimentação atipica </td>
                                 <td>Postgresql</td>
                                 <td>Regra</td>
-                                <td>www.linkParaDownloadDaRegra.com.br</td>
+                                <td><DownloadIcon/></td>
                             </tr>
                         </tbody>
                         <tbody >
                             <tr>
                                 <td>1</td>
-                                <td>MF6 - Movimentação atipica com contraparte</td>
+                                <td>MF4 - Movimentação atipica </td>
                                 <td>Postgresql</td>
                                 <td>Regra</td>
-                                <td>www.linkParaDownloadDaRegra.com.br</td>
+                                <td><DownloadIcon/></td>
                             </tr>
                         </tbody>
                         <tbody >
                             <tr>
                                 <td>1</td>
-                                <td>MF6 - Movimentação atipica com contraparte</td>
+                                <td>MF4 - Movimentação atipica </td>
                                 <td>Postgresql</td>
                                 <td>Regra</td>
-                                <td>www.linkParaDownloadDaRegra.com.br</td>
+                                <td><DownloadIcon/></td>
                             </tr>
                         </tbody>
-                        <tbody >
-                            <tr>
-                                <td>1</td>
-                                <td>MF6 - Movimentação atipica com contraparte</td>
-                                <td>Postgresql</td>
-                                <td>Regra</td>
-                                <td>www.linkParaDownloadDaRegra.com.br</td>
-                            </tr>
-                        </tbody>
-                        <tbody >
-                            <tr>
-                                <td>1</td>
-                                <td>MF6 - Movimentação atipica com contraparte</td>
-                                <td>Postgresql</td>
-                                <td>Regra</td>
-                                <td>www.linkParaDownloadDaRegra.com.br</td>
-                            </tr>
-                        </tbody>
-                        </table> 
-                        </div>
-                        
-                        
-    </div>
+                       
+                    </table>
+                </div>
 
-    <Footerbar/>
-    
-    </>);
+                <div className=" d-flex justify-content-center">
+                    <nav>
+                        <ul className="pagination">
+                            <li className="page-item">
+                                <button className="page-link">Anterior</button>
+                            </li>
+                            <li className="page-item disabled">
+                                <span className="page-link">1</span>
+                            </li>
+                            <li className="page-item disabled">
+                                <button className="page-link">Próxima</button>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+            <Footerbar />
+
+        </>);
 }
 
 export default Result;
